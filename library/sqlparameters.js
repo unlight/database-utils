94edb4ca3b7d48809c703f862f72babcc3e52d8a
iamthes <s@home.local>
2013-10-28 20:32:36 +0400

    Improved sqlparameters.js

--- library/sqlparameters.js
+++ library/sqlparameters.js
@@ -3,8 +3,7 @@
 module.exports = SqlParameters;
 
 function SqlParameters() {
-
-	this._parameters = [];
+	this.reset();
 }
 
 SqlParameters.prototype.add = function(name, value) {
@@ -12,7 +11,11 @@ SqlParameters.prototype.add = function(name, value) {
 		throw new TypeError("Add() expecting at least 1 parameter.");
 	} else if (arguments.length == 1) {
 		value = arguments[0];
-		this._parameters.push({value: value});
+		if (typeof value == "object" && value.constructor === Object) {
+			this._parameters.push(value);
+		} else {
+			this._parameters.push({value: value});
+		}
 	} else {
 		this._parameters.push({
 			name: name,
@@ -21,11 +24,17 @@ SqlParameters.prototype.add = function(name, value) {
 	}
 }
 
-SqlParameters.prototype.arrayValues = function() {
+SqlParameters.prototype.push = SqlParameters.prototype.add;
+
+SqlParameters.prototype.values = function() {
 	var result = [];
 	for (var count = this._parameters.length, i = 0; i < count; i++) {
 		var parameter = this._parameters[i];
 		result[result.length] = parameter.value;
 	}
 	return result;
+}
+
+SqlParameters.prototype.reset() {
+	this._parameters = [];
 }
--- readme.md
+++ readme.md
@@ -7,11 +7,11 @@ TODO
 ----
 - [hold, need execute] replace($table = '', $set = null, $where, $checkexisting = false)
 - [hold] history($updatefields = true, $insertfields = false)
+- parametric query with ?
 - collect values as parameters to 
 - [hold, need depends on execute] simple getcount
 - [hold, need depends on execute] simple getcountlike
 - [hold, need depends on execute] simple getwhere
-- parametric query with ?
 - write the test/functionality that is needed
 - unit tests
 - [hold] limit in update()
